e82f117fc04584347be3975fdc5743f66cd40a06
Eko <Eko@10.6.162.55>
2017-02-08 15:36:42 +0700

    Homepage and SASS

--- Web/Views/Home/index.php
+++ Web/Views/Home/index.php
@@ -1,14 +1,20 @@
-<table class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th>Nama Pengguna</th>
-                <th>Email</th>
-            </tr>
-        </thead>
+<div class="row parent-home-menu">
+    <div class="right blue-grey home-menu">
+        <div class="row">
+            <div class=""><a href="http://localhost/kurniawantaari/Web">Home</a></div>
+            <div class=""><a href="http://localhost/kurniawantaari/Web/Tutorial">Tutorial</a></div>
+            <div class=""><a href="http://localhost/kurniawantaari/Marketplace">Marketplace</a></div>
+            <div class=""><a href="http://localhost/kurniawantaari/Wedding">Wedding</a></div>
 
-        <tbody>
-            <tr>
-                <td colspan="2">{{10+2}}</td>
-            </tr>
-        </tbody>
-    </table>
+            <div class="col s2 offset-s4">
+                <a class="btn btn-small" href="https://www.facebook.com/lestari.ekowahyu"><i class="fa fa-facebook"></i></a>
+            </div>
+            <div class="col s2">
+                <a class="btn btn-small" href="https://www.github.com/kurniawantaari"><i class="fa fa-github"></i></a>
+            </div>
+            <div class="col s2">
+                <a class="btn btn-small" href="mailto:lestari.ekowahyu@gmail.com"><i class="fa fa-envelope"></i></a>        
+            </div>
+        </div>
+    </div>
+</div>
--- Web/Views/layout.php
+++ Web/Views/layout.php
@@ -6,44 +6,56 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <?php
-        /*
+        <?php /*
          * <base> digunakan untuk menentukan URL dasar dari seluruh halaman web.
          * contoh: public/css/style.css pada kode HTML akan dibaca sebagai
          * http://localhost/framework/public/css/style.css
-         */?>
+         */ ?>
         <!--<base href="http://localhost/kurniawantaari/Web/">-->
         <title>Kurniawantaari</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="resources/css/test.css"/>
-          <!--<script type="text/javascript" src="resources/js/angular.min.js"></script>-->
-          <!--<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>-->
-    </head>
-    <body ng-app>
+        <link rel="stylesheet" href="resources/css/materialize.css"/>
+        <link rel="stylesheet" href="resources/css/style.css"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>-->
+        <script type="text/javascript" src="resources/js/jquery-3.1.1.min.js"></script>
+        <script type="text/javascript" src="resources/js/materialize.min.js"></script>
+        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>-->
+        <script src="https://use.fontawesome.com/8d1cd7da0d.js"></script>
+  </head>
+    <body>
         <header>
             <nav>
-                <div class="nav-wrapper">
-                    <a href="#!" class="brand-logo-right">Logo</a>
-                    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-                    <ul class="right hide-on-med-and-down">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">Javascript</a></li>
-                        <li><a href="mobile.html">Mobile</a></li>
-                    </ul>
-                    <ul class="side-nav" id="mobile-demo">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">Javascript</a></li>
-                        <li><a href="mobile.html">Mobile</a></li>
-                    </ul>
+                <div class="nav-wrapper transparent">
+                    <a href="http://localhost/kurniawantaari/Web" class="brand-logo center materialize-green-text">Guratize</a>
+                    <a href="#" data-activates="menu" class="button-collapse right show-on-large"><i id="button-menu" class="fa fa-bars fa-2x materialize-green-text"></i></a>
+                    <ul id="menu" class="side-nav">
+                        <li><a href="http://localhost/kurniawantaari/Web">Home</a></li>
+                        <li><a href="http://localhost/kurniawantaari/Web/Tutorial">Tutorial</a></li>
+                        <li><a href="http://localhost/kurniawantaari/Marketplace">Marketplace</a></li>
+                        <li><a href="http://localhost/kurniawantaari/Wedding">Wedding</a></li>
+                    </ul> 
                 </div>
             </nav>
         </header>
+        <main>
+            <?php require($content) ?>
+        </main>
+        <footer class="page-footer">
+            <div class="container">
+                Guratize &COPY; <?php echo date("Y"); ?>
+            </div>
+        </footer>
+        <script>
+            $('.button-collapse').sideNav({
+                menuWidth: 300, // Default is 300
+                edge: 'right', // Choose the horizontal origin
+                closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                draggable: true // Choose whether you can drag to open on touch screens
+            }
+            );
 
-<div class="container mymod">
-    <?php require($content) ?>
-</div>
+        </script>
     </body>
 </html>
